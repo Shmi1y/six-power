ec30b36278b1b898006aa6b077a7a309efc64e7d
Shmi1y <582320445@qq.com>
2019-12-11 09:18:43 +0800

    整合部分代码

--- furniture-home-app/src/router/routes.js
+++ furniture-home-app/src/router/routes.js
@@ -8,7 +8,10 @@ import Fine from '../pages/Mine/Fine/Fine.vue'
 import Login from '../pages/Login/Login.vue'
 import Register from '../pages/Register/Register.vue'
 import Detalis from '../pages/Detalis/Detalis.vue'
-
+import One from '../pages/Afflatus/One/One.vue'
+import Two from '../pages/Afflatus/Two/Two.vue'
+import Three from '../pages/Afflatus/Three/Three.vue'
+import AfflatusContent from '../pages/Afflatus/AfflatusContent/AfflatusContent.vue'
 export default [
   {
     path:'/home',
@@ -22,7 +25,34 @@ export default [
     component: Afflatus,
     meta: {
       isShowFooter: true
-    }
+    },
+    children:[
+      {
+        path:'/afflatus/one',
+        component: One,
+        meta: {
+          isShowFooter: true
+        }
+      },
+      {
+        path:'/afflatus/two',
+        component: Two,
+        meta: {
+          isShowFooter: true
+        }
+      },
+      {
+        path:'/afflatus/three',
+        component: Three,
+        meta: {
+          isShowFooter: true
+        }
+      },
+      {
+        path:'/afflatus',
+        redirect:'/afflatus/one'
+      }
+    ]
   },
   {
     path:'/news',
@@ -65,4 +95,8 @@ export default [
     path:'/',
     redirect:'/home'
   },
+  {
+    path:'/afflatus_content',
+    component:AfflatusContent
+  }
 ]
